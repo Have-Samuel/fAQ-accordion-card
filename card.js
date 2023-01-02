6d682285010c9a50d31ee6bdb0bdfb2b0bc6b50b
Have Samuel <samiehave9@gmail.com>
2023-01-02 23:16:37 +0300

    Adding content to the HTML and making the arrows functionality

---
+++ card.js
@@ -0,0 +1,18 @@
+/* When the user clicks on the arrow,
+toggle between hiding and showing the content */
+const dropdown = () => {
+  document.getElementById('myDropdown').classList.toggle('show');
+};
+
+// Close the content if the user clicks outside of it
+window.onclick = (event) => {
+  if (!event.target.matches('.drop')) {
+    let dropdown = document.getElementsByClassName('answer');
+    for (let i = 0; i < dropdown.length; i++) {
+      let openDropdown = dropdown[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
