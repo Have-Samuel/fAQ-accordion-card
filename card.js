b832829ffa34ab29ce7020d3aad610a39e6b0e02
Have Samuel <samiehave9@gmail.com>
2023-01-04 20:46:16 +0300

    Changing the functionality

--- card.js
+++ card.js
@@ -1,11 +1,11 @@
 /* When the user clicks on the arrow,
 toggle between hiding and showing the content */
-function myFunction() {
+const myFunction = () => {
   document.getElementById('myDropdown').classList.toggle('show');
-}
+};
 
 // Close the content if the user clicks outside of it
-window.onclick = (event) => {
+document.onclick = (event) => {
   if (!event.target.matches('.drop')) {
     let dropdowns = document.getElementsByClassName('answer');
     for (let i = 0; i < dropdowns.length; i++) {
