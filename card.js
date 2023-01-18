fe7e3c807bf064d4b9ea7a46fa6eb1c093685206
Have Samuel <samiehave9@gmail.com>
2023-01-19 02:18:00 +0300

    Styling and Making the clicking functionality

--- card.js
+++ card.js
@@ -1,18 +1,8 @@
-/* When the user clicks on the arrow,
-toggle between hiding and showing the content */
-const myFunction = () => {
-  document.getElementById('myDropdown').classList.toggle('show');
-};
+const solutions = document.querySelectorAll('.qus-ans');
 
-// Close the content if the user clicks outside of it
-document.onclick = (event) => {
-  if (!event.target.matches('.drop')) {
-    let dropdowns = document.getElementsByClassName('answer');
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+solutions.forEach((solution) => {
+  solution.addEventListener('click', () => {
+    solution.classList.value.includes('visible')
+    solution.classList.toggle('visible')
+  })
+});
